415a497c4479194d42184dd39c6f6324cfb44f91
Subodh Nikumbh <60419366+subodhnikumbh123@users.noreply.github.com>
2026-09-02 16:37:16 -0500

    Add header comments to ddl_bronze.sql
    Added documentation header to the DDL script for bronze tables.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,4 +1,13 @@
---this script to re-define the DDL structure of 'bronze' Tables
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+    Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 
 CREATE TABLE bronze.crm_cust_info (
